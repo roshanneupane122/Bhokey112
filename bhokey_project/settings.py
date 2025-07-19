e9e0124c8fdfe29e9ef48b9b984b4ec5ec3e5e49
Roshan neupane <124055798+roshanneupane122@users.noreply.github.com>
2025-07-19 23:46:03 +0545

    Update settings.py

--- bhokey_project/settings.py
+++ bhokey_project/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://bhokey-static.onrender.com/'
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
