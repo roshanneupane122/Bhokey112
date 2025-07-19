808243c14b1e96a519861c637fcd8620b9d0f521
Roshan neupane <124055798+roshanneupane122@users.noreply.github.com>
2025-07-19 23:26:08 +0545

    Update urls.py

--- foodshare/urls.py
+++ foodshare/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
+
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -15,5 +15,4 @@ urlpatterns = [
  
    
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
