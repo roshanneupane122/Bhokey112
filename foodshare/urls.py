41be50206aeb0ba422fda5efb7b9783d5d240b08
Roshan neupane <124055798+roshanneupane122@users.noreply.github.com>
2025-07-20 09:56:30 +0545

    Update urls.py

--- foodshare/urls.py
+++ foodshare/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('edit/<int:pk>/', views.edit_post, name='edit_post'),
     path('delete/<int:pk>/', views.delete_post, name='delete_post'),
     path('pickup/<int:pk>/', views.pickup_view, name='pickup_view'),
+    path('debug_foodposts/', debug_foodposts, name='debug_foodposts'),
 
    
  
