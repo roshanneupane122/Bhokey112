d7d1e35b4a21ccf54bf060bf58317cc0590a1540
Roshan neupane <124055798+roshanneupane122@users.noreply.github.com>
2025-07-20 09:40:46 +0545

    Update settings.py

--- bhokey_project/settings.py
+++ bhokey_project/settings.py
@@ -127,8 +127,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
